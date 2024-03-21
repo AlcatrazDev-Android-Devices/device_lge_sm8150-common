848f6ee9924afc1b2eb7b905f1085755989265a1
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    sm8150-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618

--- sm8150.mk
+++ sm8150.mk
@@ -479,10 +479,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.lge_sm8150
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # Update engine
 PRODUCT_HOST_PACKAGES += \
     brillo_update_payload
@@ -497,7 +493,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Vendor service manager
 PRODUCT_PACKAGES += \

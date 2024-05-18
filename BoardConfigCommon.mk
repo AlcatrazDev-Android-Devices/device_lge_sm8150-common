93299b3b17db26813dfcfc93cfd12cb41dc1ecf5
Han Sol Jin <hansol@hansol.ca>
2024-05-18 09:42:32 -0700

    sm8150-common: explicitly include common Lineage libperfmgr sepolicy
    Change-Id: I37cfc4de02af9f7711bf1fc32aabbbe539017c24

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,6 +167,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/lge/sepolicy/SEPolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor

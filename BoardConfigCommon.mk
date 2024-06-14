651a114abb800a8d8066b4ce99a00407d8c1051c
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:50:24 +0300

    sm8150-common: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,6 +88,7 @@ BOARD_HAVE_QCOM_FM := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
